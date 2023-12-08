a2f062a1590ab66ce6a0c7f34529ab085fffada7
ahmed-M32 <am.3443rrrr@gmail.com>
2023-12-08 05:32:38 +0200

    d

--- src/components/navbar/nav.jsx
+++ src/components/navbar/nav.jsx
@@ -9,7 +9,7 @@ function Navbar(props) {
 			<div className="es">
 				<Link to = {"e-commerce-project/"}></Link>
 				<img
-					src="src\assets\shopping-and-ecommerce29.png"
+					src="https://raw.githubusercontent.com/ahmed-M32/e-commerce-project/main/src/assets/shopping-and-ecommerce29.png"
 					alt=""
 					className="logo"
 				/>
@@ -18,7 +18,7 @@ function Navbar(props) {
 				<div className="search">
 					<input className="bar" type="text" placeholder="search for Products or Categories..." />
 					<button className="searchB"> 
-						<img src="https://raw.githubusercontent.com/ahmed-M32/e-commerce-project/mainsrc/assets/pngwing.com.png" className ="searchi" alt="" />
+						<img src="https://raw.githubusercontent.com/ahmed-M32/e-commerce-project/main/src/assets/pngwing.com.png" className ="searchi" alt="" />
 					</button>
 				</div>
 				<div className="cart">
